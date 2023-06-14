f382ab9a394a54a1e793cd23ac97067d432b9212
Josh <jthalappillil7@gmail.com>
2023-06-14 11:08:48 +1000

    Added logic. Currently displays game in console.log

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
-function playGame(){
-    const computerSelection = getComputerChoice();
-    
-}
+const gameContainer = document.getElementById("game");
 
-getComputerChoice();
+gameContainer.addEventListener("click", function(event){
+    if(event.target.matches("button")){
+        const userSelection = event.target.dataset.choice;
+        console.log("User chose:", userSelection);
+
+        const computerSelection = getComputerChoice();
+        console.log("Computer chose:", computerSelection);
+
+        const winner = getWinner(userSelection, computerSelection);
+        console.log(winner);
+
+    }
+})
+
+
+const computerSelection = getComputerChoice();
+
 function getComputerChoice(){
     const choices = ["Rock", "Paper", "Scissors"];
     const randomChoice = Math.floor(Math.random() * choices.length); 
@@ -12,3 +25,20 @@ function getComputerChoice(){
     return choices[randomChoice];
 }
 
+function getWinner(userSelection, computerSelection){
+    if (userSelection === computerSelection){
+        return "It's a tie!";
+    }
+    else if (userSelection === "Rock" && computerSelection === "Scissors"){
+        return "You win!";
+    }
+    else if (userSelection === "Paper" && computerSelection === "Rock"){
+        return "You win!";
+    }
+    else if (userSelection === "Scissors" && computerSelection === "Paper"){
+        return "You win!";
+    }
+    else{
+        return "You lose!";
+    }
+}
